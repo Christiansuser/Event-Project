671645a7ea50d7617896f3f6cc0cd94e8007540d
Christiansuser <c.agbor11189@stu.allstarcode.org>
2024-07-22 12:30:23 -0400

    added images

---
+++ script.js
@@ -0,0 +1,45 @@
+let colorAqua = document.getElementById('aqua');
+let colorTeal = document.getElementById('teal');
+let colorPink = document.getElementById('pink');
+let colorPurple = document.getElementById('Purple');
+let colorColorless = document.getElementById('Colorless');
+
+Aqua.onclick = function() {   
+    
+    if (Aqua.style.backgroundcolor != "aqua") {
+        box.syle.backgroundcolor = "aqua";
+        box.innerHTML = "aqua";
+    }
+}
+
+Teal.onclick = function() {   
+    
+    if (teal.style.backgroundcolor != "teal") {
+        box.syle.backgroundcolor = "teal";
+        box.innerHTML = "teal";
+    }
+}
+
+Pink.onclick = function() {   
+    
+    if (Pink.style.backgroundcolor != "pink") {
+        box.syle.backgroundcolor = "pink";
+        box.innerHTML = "pink";
+    }
+}
+
+Purple.onclick = function() {   
+    
+    if (Aqua.style.backgroundcolor != "purple") {
+        box.syle.backgroundcolor = "purple";
+        box.innerHTML = "purple";
+    }
+}
+
+Colorless.onclick = function() {   
+    
+    if (Aqua.style.backgroundcolor != "colorless") {
+        box.syle.backgroundcolor = "colorless";
+        box.innerHTML = "colorless";
+    }
+}
